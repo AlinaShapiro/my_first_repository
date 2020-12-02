afdc66080bba99862b16173cf8bd802893a1db59
AlinaShapiro <adshapiro_2@edu.hse.ru>
2020-12-02 11:42:13 +0300

    02.12.2020. 1 Задание

---
+++ min.py
@@ -0,0 +1,14 @@
+print("Write the first number: ")
+a = int(input())
+print("Write the second number:")
+b = int(input())
+print("Write the third number:")
+c = int(input())
+print("Write the forth number:")
+d = int(input())
+minimum = min(a,b,c,d)
+print("min: ",minimum)
+
+
+
+
